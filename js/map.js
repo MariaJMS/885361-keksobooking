d5418d95f2f10366a5e9ad8ccc85a4bd1e7f5781
Maria <venema.jms@gmail.com>
2018-11-30 16:08:40 +0700

    Проверка количества гостей при выборе комнаты

--- js/map.js
+++ js/map.js
@@ -238,3 +238,39 @@ var onMapPinMainMouseUp = function () {
 };
 
 mapPinMain.addEventListener('mouseup', onMapPinMainMouseUp);
+
+// установка соответствия количества гостей количеству комнат
+var roomNumber = document.querySelector('#room_number');
+var capacity = document.querySelector('#capacity');
+
+var maxCapacity = {
+  '1': 1,
+  '2': 2,
+  '3': 3,
+  '100': 0
+};
+
+var validСapacity = function () {
+  var roomsNumber = roomNumber.value;
+  var guests = parseInt(capacity.value, 10);
+  var possibleCapacity = maxCapacity[roomsNumber];
+  if (guests > possibleCapacity) {
+    capacity.setAttribute('disable', true);
+    capacity.setCustomValidity('Недопустимое количество мест для выбранного числа комнат');
+  } else if (guests === 0 && possibleCapacity > 0) {
+    capacity.setAttribute('disable', true);
+    capacity.setCustomValidity('Комнаты не для гостей');
+  } else {
+    capacity.setAttribute('disable', false);
+    capacity.setCustomValidity('');
+  }
+};
+
+validСapacity();
+
+var onCapacityChange = function () {
+  validСapacity();
+};
+
+capacity.addEventListener('change', onCapacityChange);
+roomNumber.addEventListener('change', onCapacityChange);
